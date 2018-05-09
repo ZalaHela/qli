0e982bbf74703ad4af21abf52f7a962b97991e0d
DrPenknife <rkozik@utp.edu.pl>
2018-05-09 09:10:23 +0200

    db-admin *.bat tools

---
+++ admintools/init_db.bat
@@ -0,0 +1,3 @@
+pause
+..\\..\\..\\mysql\\bin\\mysql.exe -u root -p -e "source init_db.sql"
+pause
---
+++ admintools/init_db.sql
@@ -0,0 +1,4 @@
+show databases;
+drop database if exists apka;
+source ../db/create.txt
+source ../db/alters.txt
